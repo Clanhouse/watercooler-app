71e1738a37fe8e6d6803b39b5516db541d2e967a
Milosz <m.branewicz@wp.pl>
2021-05-29 16:11:30 +0200

    Add props and props validation for button component

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './button.css';
 
-export default function Button() {
-  return <div></div>;
+import Loader from '../Loader/Loader';
+
+export default function Button({
+  buttonSize,
+  buttonLink,
+  isLoading,
+  onClick,
+  action,
+}) {
+  return (
+    <button
+      type={action ? 'button' : 'submit'}
+      onClick={onClick}
+      className={buttonSize}
+    >
+      {isLoading ? <Loader /> : buttonLink}
+    </button>
+  );
 }
+Button.defaultProps = {
+  action: true,
+  buttonSize: 'small',
+  buttonLink: 'click',
+  isLoading: false,
+};
+
+Button.propTypes = {
+  buttonSize: PropTypes.string,
+  action: PropTypes.bool,
+  buttonLink: PropTypes.string,
+  isLoading: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+};
