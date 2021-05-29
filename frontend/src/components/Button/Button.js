1eef6f63cb29e5d487150364e8b402eaac94f45f
Milosz <m.branewicz@wp.pl>
2021-05-29 23:04:21 +0200

    Fix eslint problems [EndOfLine:auto]

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './button.css';
-
+import PropTypes from 'prop-types';
 import Loader from '../Loader/Loader';
 
 export default function Button({
