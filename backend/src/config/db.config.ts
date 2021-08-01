ff902b6e6533a464c3b6acb91a65ff2ffaf37918
Tomasz Jankowski <tomasz.ad.jankowski@gmail.com>
2021-08-01 15:27:03 +0200

    Fix for ORM config

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,15 +1,19 @@
 import { ConnectionOptions } from "typeorm";
 import * as dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
+
+const entitiesDir = path.join(__dirname, "..", "db", "entities");
 
 const dbConnnectionOptions: ConnectionOptions = {
   type: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PWD,
-  entities: [__dirname + "db/entities/*.entity.{js,ts}"],
+  entities: [`${entitiesDir}/*.entity.{js,ts}`],
   synchronize: process.env.DB_SYNC === "true"
 }
 
